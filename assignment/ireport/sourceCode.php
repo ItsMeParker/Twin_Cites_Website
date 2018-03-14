466077a093dac7cf2e9c24ab27cbd0d7eaa255fb
Connor Parker <Connor2.Parker@live.uwe.ac.uk>
2018-03-14 23:07:03 +0000

    final commit after hand in with submission files ind and grp Work and minor change to css

---
+++ assignment/ireport/sourceCode.php
@@ -0,0 +1,15 @@
+<?php
+
+echo (" <h2>Connor Parker database_xml_xsd</h2><ul>");
+    echo("<li><h3>fet15021296.xml</h3><p>");
+    highlight_file('../source/database_xml_xsd/fet15021296.xml');
+    echo("</p></li>");
+
+    echo("<li><h3>fet15021296.xsd</h3><p>");
+    highlight_file('../source/database_xml_xsd/fet15021296.xsd');
+    echo("</p></li>");
+echo("</ul>");
+
+
+
+?>
